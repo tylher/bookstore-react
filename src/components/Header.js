6eb92b561b7867e47540a8198ee6cb94cf4803bf
Damola Adediran <tylher123@gmail.com>
2022-04-30 11:08:46 +0100

    add Header component

---
+++ src/components/Header.js
@@ -0,0 +1,29 @@
+import { Link } from "react-router-dom";
+const Header = () => {
+  const links = [
+    {
+      id: 1,
+      name: "Books",
+      path: "/Books",
+    },
+    { id: 2, name: "Categories", path: "/Categories" },
+  ];
+  return (
+    <header>
+      <h1>Bookstore CMS</h1>
+      <nav>
+        <ul>
+          {links.map((link) => {
+            return (
+              <li>
+                <Link to={link.path}>{link.name}</Link>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header
