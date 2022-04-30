ba6eb9c36c4d3bf182f411e415c95c6782dd4754
Damola Adediran <tylher123@gmail.com>
2022-04-30 11:45:42 +0100

    Add 'add book' button

--- src/components/InputBook.js
+++ src/components/InputBook.js
@@ -2,6 +2,7 @@ const InputBook = () => (
   <form>
     <input type="text" name="title" placeholder="Enter book title" />
     <input type="text" name="author" placeholder="Enter book author" />
+    <button type="submit">Add book</button>
   </form>
 );
 
