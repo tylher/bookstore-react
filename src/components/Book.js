b0c2349a70e3aede0732cec442dd167194146189
Damola Adediran <tylher123@gmail.com>
2022-05-06 19:14:02 +0100

    Add style files to project

--- src/components/Book.js
+++ src/components/Book.js
@@ -3,26 +3,55 @@ import { useDispatch } from 'react-redux';
 import { removeBookAPI } from '../redux/books/books';
 
 const Book = (props) => {
-  const { title, author, id } = props;
+  const {
+    title, author, id, category,
+  } = props;
   const dispatch = useDispatch();
   const handleClick = () => {
     dispatch(removeBookAPI(id));
   };
 
+  const randomNum = Math.floor(Math.random() * 101);
+
+  const progressBar = {
+    backgroundImage: `conic-gradient( #307bbe , #379cf6 ${randomNum}%, white ${randomNum}% )`,
+  };
+
   return (
-    <>
-      <h2>{title}</h2>
-      <p>{author}</p>
-      <button type="button" onClick={handleClick}>
-        Remove
-      </button>
-    </>
+    <div className="card">
+      <div className="book-details">
+        <span>{category}</span>
+        <h2>{title}</h2>
+        <p>{author}</p>
+        <div className="btn-group">
+          <button type="button">Comments</button>
+          <button type="button" onClick={handleClick}>
+            Remove
+          </button>
+          <button type="button">Edit</button>
+        </div>
+      </div>
+
+      <div className="completed">
+        <div className="outer-circle" style={progressBar}>
+          <div className="inner-circle" />
+        </div>
+        <div className="completed-text">
+          <h2 className="percentage">
+            {randomNum}
+            %
+          </h2>
+          <span>completed</span>
+        </div>
+      </div>
+    </div>
   );
 };
 
 Book.propTypes = {
   title: PropTypes.string.isRequired,
   author: PropTypes.string.isRequired,
+  category: PropTypes.string.isRequired,
   id: PropTypes.string.isRequired,
 };
 export default Book;
--- src/components/Books.js
+++ src/components/Books.js
@@ -1,16 +1,19 @@
 import { useSelector } from 'react-redux';
 import Book from './Book';
 import InputBook from './InputBook';
+import './Books.css';
 
 const Books = () => {
   const bookList = useSelector((state) => state.books);
   return (
     <>
       {bookList.map((book) => {
-        const { id, author, title } = book;
+        const {
+          id, author, title, category,
+        } = book;
         return (
           <div key={id}>
-            <Book author={author} title={title} id={id} />
+            <Book author={author} title={title} id={id} category={category} />
           </div>
         );
       })}
